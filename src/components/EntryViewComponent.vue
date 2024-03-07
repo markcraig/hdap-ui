9df8894c8c96139569ae4d640b466c18d2238a24
Matthew Swift <matthew.swift@forgerock.com>
2024-03-07 17:04:52 +0100

    Fix race condition in entry viewer

--- src/components/EntryViewComponent.vue
+++ src/components/EntryViewComponent.vue
@@ -2,23 +2,36 @@
 import {useRoute} from 'vue-router'
 import {ref} from "vue";
 import {useAuthStateStore} from '@/stores/state'
-import { JsonForms } from '@jsonforms/vue';
-import { vuetifyRenderers } from '@jsonforms/vue-vuetify';
+import {JsonForms} from '@jsonforms/vue';
+import {vuetifyRenderers} from '@jsonforms/vue-vuetify';
 
 const renderers = [
   ...vuetifyRenderers,
   // here you can add custom renderers
 ];
 const frozenRenderers = Object.freeze(renderers)
-
-let dn = useRoute().params.dn
-const entry = ref({})
-const schema = ref({})
-const objectClass = ref({})
 const authState = useAuthStateStore()
-const request = authState.addAuthzHeader(new Request("/hdap/" + dn, {method: "GET"}))
-const schemaRequest = authState.addAuthzHeader(new Request("/hdap/" + dn + "?_action=schema", {method: "POST"}))
 const uischema = ref(null)
+
+// Fetch the entry and its schema, making sure to set everything atomically on completion
+// in order to avoid attempting to render the entry without schema.
+const content = ref({})
+let dn = useRoute().params.dn
+const entryRequest = authState.addAuthzHeader(new Request("/hdap/" + dn, {method: "GET"}))
+const schemaRequest = authState.addAuthzHeader(new Request("/hdap/" + dn + "?_action=schema", {method: "POST"}))
+const schema = ref({})
+fetch(schemaRequest)
+    .then((r) => r.json())
+    .then((j) => schema.value = j)
+    .then((_) => fetch(entryRequest))
+    .then((r) => r.json())
+    .then((j) => {
+      content.value = {
+        schema: schema.value,
+        objectClass: resolveObjectClass(j.objectClass),
+        entry: j
+      }
+    })
 
 function resolveObjectClass(objectClasses) {
   for (const oc of objectClasses) {
@@ -33,80 +46,68 @@ function resolveObjectClass(objectClasses) {
 }
 
 function onChange(event) {
-  entry.value = event.data;
+  content.value.entry = event.data;
 }
-
-fetch(request)
-    .then((r) => r.json())
-    .then((j) => {
-      entry.value = j
-      objectClass.value = resolveObjectClass(j.objectClass)
-    })
-
-
-fetch(schemaRequest)
-    .then((r) => r.json())
-    .then((j) => schema.value = j)
 </script>
 
 <template>
-  <div class="view" v-if="objectClass === 'person'">
-    <h1 class="green">Viewing {{ entry.cn[0] }}</h1>
+  <div class="view" v-if="content.objectClass === 'person'">
+    <h1 class="green">Viewing {{ content.entry.cn[0] }}</h1>
     <v-form readonly>
       <v-container>
         <v-row>
           <v-col>
-            <v-text-field v-model="entry.givenName" label="First name"/>
+            <v-text-field v-model="content.entry.givenName" label="First name"/>
           </v-col>
           <v-col>
-            <v-text-field v-model="entry.sn" label="Last name"/>
+            <v-text-field v-model="content.entry.sn" label="Last name"/>
           </v-col>
         </v-row>
         <v-row>
           <v-col>
-            <v-text-field v-model="entry.mail" label="E-mail"/>
+            <v-text-field v-model="content.entry.mail" label="E-mail"/>
           </v-col>
         </v-row>
       </v-container>
     </v-form>
     <json-forms
-        :data="entry"
-        :schema="schema"
+        :data="content.entry"
+        :schema="content.schema"
         :uischema="uischema"
         :renderers="frozenRenderers"
         @change="onChange"
     />
   </div>
-  <div class="view" v-if="objectClass === 'groupOfUniqueNames'">
-    <h1 class="green">Viewing group {{ entry.cn[0] }}</h1>
+  <div class="view" v-if="content.objectClass === 'groupOfUniqueNames'">
+    <h1 class="green">Viewing group {{ content.entry.cn[0] }}</h1>
     <v-table>
       <tbody>
-      <tr v-for="item in entry.uniqueMember">
+      <tr v-for="item in content.entry.uniqueMember">
         <td><a v-bind:href="`/view/${item}`">{{ item }}</a></td>
       </tr>
       </tbody>
     </v-table>
   </div>
-  <div class="view" v-if="objectClass === 'groupOfNames'">
-    <h1 class="green">Viewing group {{ entry.cn[0] }}</h1>
+  <div class="view" v-if="content.objectClass === 'groupOfNames'">
+    <h1 class="green">Viewing group {{ content.entry.cn[0] }}</h1>
     <v-table>
       <tbody>
-      <tr v-for="item in entry.member">
+      <tr v-for="item in content.entry.member">
         <td><a v-bind:href="`/view/${item}`">{{ item }}</a></td>
       </tr>
       </tbody>
     </v-table>
   </div>
-  <div class="view" v-if="objectClass === 'unknown'">
+  <div class="view" v-if="content.objectClass === 'unknown'">
     <h1 class="green">Viewing {{ dn }}</h1>
     <json-forms
-        :data="entry"
-        :schema="schema"
+        :data="content.entry"
+        :schema="content.schema"
         :uischema="uischema"
         :renderers="frozenRenderers"
         @change="onChange"
     />
-    <p>{{ entry }}</p>
+    <p>{{ content.entry }}</p>
   </div>
 </template>
 
